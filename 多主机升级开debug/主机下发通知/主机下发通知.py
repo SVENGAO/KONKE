27cc7389434d03958704e50508eaa89b78baefea
svengao <gsf0551@126.com>
2022-04-29 16:27:04 +0800

    Changes

--- 多主机升级开debug/主机下发通知/主机下发通知.py
+++ 多主机升级开debug/主机下发通知/主机下发通知.py
@@ -14,10 +14,7 @@ def Notion_CCU():
 	a = json.load(f)
 
 	for i in a:  # I 代表主机号
-		# print(i)
 		if i != '':
-			# logging.info(i)
-			# 测试服
 			if a[i] == 'test':
 				logging.info("测试环境主机%s", i)
 				url = "http://ops-test.nj-ikonke.site:1080/api/info/ccu" + i + "/notifyCcuUpgrade"
@@ -26,9 +23,8 @@ def Notion_CCU():
 				url = "https://iot-ops.ikonke.com/api/info/ccu/" + i + "/notifyCcuUpgrade"
 			r = requests.get(url).json()
 			if 'code' in r:
-				# print(r['code'])
 				if r['code'] == 200:
-					logging.info('%s下发通知成功', i)
+					logging.info('%s下发升级通知成功', i)
 
 
 def Getversion_CCU():
@@ -40,9 +36,7 @@ def Getversion_CCU():
 	a = json.load(f)
 
 	for i in a:  # I 代表主机号
-		# print(i)
 		if i != '':
-			# logging.info(i)
 			if a[i] == 'test':
 				logging.info("测试环境主机%s", i)
 				url1 = "http://ops-test.nj-ikonke.site:1080/api/info/ccu/ccuInfo"
@@ -51,9 +45,7 @@ def Getversion_CCU():
 				url1 = "https://iot-ops.ikonke.com/api/info/ccu/ccuInfo"
 			data = {"projectId": "1", "ccuId": i}
 			r2 = requests.post(url1, json=data).json()
-			# print(r2)
 			if 'code' in r2:
-				# print(code)
 				if r2['code'] == 200:
 					if 'data' in r2:
 						if 'installVersion' in r2['data']['baseInfo']:
