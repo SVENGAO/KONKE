312d6c861d0bfea1e697a76ee926f16d7105c252
svengao <gsf0551@126.com>
2022-05-11 10:51:12 +0800

    Changes

--- 运维平台发送报文/多个主机循环发送.py
+++ 运维平台发送报文/多个主机循环发送.py
@@ -15,15 +15,20 @@ data = GET_CCU_INFO
 
 f = open('多个主机循环发送.json', "r")
 a = json.load(f)
-for index in a:  # I 代表主机号
-	logging.info('获取到的CCU信息%s', index)
-	if index:
-		r = Yunweipingtai.Sent_Sockit(index, data)
-		if 'status' in r:
-			if r['status'] == 'success':
-				logging.info("接受到的主机%s回复opcode%s信息:%s" % (index, data['opcode'], r['arg']))
-			elif r['status'] == 'timeout':
-				logging.info("接受到的主机%s回复opcode%s信息超时，主机可能不在线,错误信息%s" % (index, data['opcode'], r['errorinfo']))
+for CCU in a:  # I 代表主机号
+	logging.info('获取到的CCU信息%s', CCU)
+	if CCU:
+		count = 2   # count 代表循环次数
+		for i in (1, count):
+			print(i)
+			r = Yunweipingtai.Sent_Sockit(CCU, data)
+			if 'status' in r:
+				if r['status'] == 'success':
+					logging.info("接受到的主机%s回复opcode%s信息:%s" % (CCU, data['opcode'], r['arg']))
+				elif r['status'] == 'timeout':
+					logging.error("接受到的主机%s回复opcode%s信息超时，主机可能不在线,错误信息%s" % (CCU, data['opcode'], r['errorinfo']))
+				elif r['status'] == 500:
+					logging.error("接受到的主机%s回复opcode%s信息:%s" % (CCU, data['opcode'], r['error']))
 			else:
-				logging.error("接受到的主机%s回复opcode%s信息:%s" % (index, data['opcode'], r['error']))
-		logging.info("%s主机第一次获取消息", index)
+				logging.error("接受到的主机%s回复opcode%s信息:%s" % (CCU, data['opcode'], r))
+			logging.info("%s主机第%s次获取消息" % (CCU, i))
